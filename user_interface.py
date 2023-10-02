33e675d125e6585fb33a49fe0388d8155b2798d6
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 08:09:58 +0200

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -6,3 +6,7 @@ def setup_ui_elements():
 
 def show_instructions():
     st.info("Instructions: Enter a course review in the text area and click on 'Analyze' to get the sentiment analysis and additional insights.")
+import streamlit as st
+
+
+
