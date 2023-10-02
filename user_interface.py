d110d05a79b66675e675677a03870ba2c60903e4
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 11:23:18 +0200

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,6 @@
 import streamlit as st
 
-def setup_ui_elements():
-    st.sidebar.header('About App')
-    st.sidebar.write('This app uses advanced NLP models to analyze sentiments of student reviews.')
+
 
 def show_instructions():
     st.info("Instructions: Enter a course review in the text area and click on 'Analyze' to get the sentiment analysis and additional insights.")
