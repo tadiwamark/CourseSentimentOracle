29b92ec754ba66d82ff54b8f8676592c6bf02abc
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 08:07:48 +0200

    Update feedback.py

--- feedback.py
+++ feedback.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
+
 def collect_feedback(user_input, sentiment_result, additional_features):
-    feedback = st.text_area("Any feedback on the analysis?")
+    """Collect user feedback on the sentiment analysis results and additional NLP features."""
+    feedback = st.text_area("Provide feedback on the analysis:")
     if st.button("Submit Feedback"):
-        # Logic to store feedback, for example in a database or a file.
-        st.success("Thank you for your feedback!")
+        if feedback:
+            st.success("Thank you for your feedback!")
+        else:
+            st.warning("Feedback field is empty!")
