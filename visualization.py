d643563ddc0c123eed183708d3aa527c01b08930
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 11:30:15 +0200

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -6,4 +6,3 @@ def display_visualizations(sentiment_result, additional_features):
     st.subheader("Sentiment Analysis Result:")
     st.write(sentiment_result)
     
-    # You can add more visualizations (e.g. graphs, charts) related to the sentiment analysis results and additional NLP features.
