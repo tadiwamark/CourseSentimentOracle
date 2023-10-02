202b171caa85c0c2f9b006517d0b56a4e69ba553
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 09:59:33 +0200

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -6,22 +6,28 @@ import nltk
 def analyze_sentiment(review_text):
     """Sentiment Analysis."""
     preprocessed_text = preprocess_text(review_text)  # preprocess text before analysis
+    conversation = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"The sentiment of this review is: {preprocessed_text}"}
+    ]
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            temperature=0.5,
+            max_tokens=100
+        )
+        sentiment_result = response['choices'][0]['message']['content']
     
-    response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=f"This is a review: '{preprocessed_text}'. The sentiment of this review is:",
-        temperature=0.5,
-        max_tokens=100
-    )
-    sentiment_result = response.choices[0].text.strip() 
-    
-    # Additional NLP Tasks and Features extraction
-    additional_features = {
-        "keywords": ["Example", "Keyword"],
-        "entities": ["Example", "Entity"]
-    }
-    
-    return sentiment_result, additional_features
+        # Additional NLP Tasks and Features extraction
+        additional_features = {
+            "keywords": ["Example", "Keyword"],
+            "entities": ["Example", "Entity"]
+        }
+        
+        return sentiment_result, additional_features
+     except openai.error.OpenAIError as e:
+        return f"Error: {e}", None
 
 
 def additional_nlp_features(features):
