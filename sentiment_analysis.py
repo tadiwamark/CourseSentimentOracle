20d1ae0c2cf17575aa8d6a978420155224c6a88c
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 10:22:08 +0200

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,6 +1,7 @@
 import openai
 from text_preprocessor import preprocess_text
 import nltk
+import streamlit as st
 
 
 def analyze_sentiment(review_text):
@@ -18,21 +19,21 @@ def analyze_sentiment(review_text):
             max_tokens=100
         )
         sentiment_result = response['choices'][0]['message']['content']
-    
+
         # Additional NLP Tasks and Features extraction
         additional_features = {
             "keywords": ["Example", "Keyword"],
             "entities": ["Example", "Entity"]
         }
-        
-        return sentiment_result, additional_features
-    
 
+        return sentiment_result, additional_features
+    except Exception as e:
+        return str(e), None  # Returning error string and None for additional_features if there is an exception
 
 def additional_nlp_features(features):
     """Display additional NLP features."""
     st.subheader("Extracted Keywords:")
     st.write(", ".join(features.get("keywords", [])))
-    
+
     st.subheader("Recognized Entities:")
     st.write(", ".join(features.get("entities", [])))
