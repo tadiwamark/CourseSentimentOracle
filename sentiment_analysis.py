f548c4ef64c3bab621e16fa01386218a95e5e2be
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 10:31:49 +0200

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -4,9 +4,26 @@ import nltk
 import streamlit as st
 
 
+import spacy
+from collections import Counter
+
+# Load spaCy model
+nlp = spacy.load('en_core_web_sm')
+
 def analyze_sentiment(review_text):
     """Sentiment Analysis."""
     preprocessed_text = preprocess_text(review_text)  # preprocess text before analysis
+    
+    # Performing NLP using spaCy
+    doc = nlp(preprocessed_text)
+
+    # Extracting entities
+    entities = [ent.text for ent in doc.ents]
+
+    # Extracting keywords using noun chunks
+    keywords = [chunk.text for chunk in doc.noun_chunks]
+
+    # Analyzing sentiment using OpenAI
     conversation = [
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": f"The sentiment of this review is: {preprocessed_text}"}
@@ -19,14 +36,15 @@ def analyze_sentiment(review_text):
             max_tokens=100
         )
         sentiment_result = response['choices'][0]['message']['content']
-
+    
         # Additional NLP Tasks and Features extraction
         additional_features = {
-            "keywords": ["Example", "Keyword"],
-            "entities": ["Example", "Entity"]
+            "keywords": keywords,
+            "entities": entities
         }
-
+        
         return sentiment_result, additional_features
+
     except Exception as e:
         return str(e), None  # Returning error string and None for additional_features if there is an exception
 
