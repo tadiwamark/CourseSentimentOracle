2e40d8bc534244f63a439f5c5f4d6b0c10ce2240
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 07:19:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,39 @@
+# app.py
 import streamlit as st
-import openai
-from preprocessing import preprocess_text # Importing our custom-made preprocessing function
-import SessionState
+from authentication import authenticate_user
+from sentiment_analysis import analyze_sentiment, additional_nlp_features
+from visualization import display_visualizations
+from feedback import collect_feedback
+from user_interface import setup_ui_elements, show_instructions
 
-# Initialize session state
-session_state = SessionState.get(api_key="", reviews=[])
-
-# Select Page
-page = st.sidebar.selectbox("Choose a page", ["Enter Review", "Analyze Reviews"])
-
-# Page 1: Enter Review
-if page == "Enter Review":
-    st.header('Enter a new Review')
-
-    # Input for New Review
-    new_review = st.text_area("Enter your review here:")
+def main():
+    st.title('Advanced NLP Sentiment Analysis App')
     
-    if st.button('Submit Review'):
-        if new_review:
-            session_state.reviews.append(new_review)
-            st.success("Review Submitted Successfully!")
-        else:
-            st.warning("Please enter a review before submitting.")
-
-# Page 2: View and Analyze Reviews
-elif page == "Analyze Reviews":
-    st.header('Analyze Reviews in Real-Time')
-
-    # API Key Input
-    if not session_state.api_key:
-        session_state.api_key = st.text_input("Enter your OpenAI API Key:")
-        if session_state.api_key:
-            openai.api_key = session_state.api_key
-    else:
-        openai.api_key = session_state.api_key
+    # Authenticate User
+    user = authenticate_user()  
     
-    # If API key is set, display and analyze reviews
-    if openai.api_key:
-        if session_state.reviews:
-            review = st.selectbox('Choose a review to analyze:', session_state.reviews)
+    # Setup UI Elements and Instructions
+    setup_ui_elements()
+    show_instructions()
+    
+    # User Input
+    user_input = st.text_area("Enter the review:")
+    if st.button('Analyze'):
+        if user_input:
+            # Sentiment Analysis
+            sentiment_result, additional_features = analyze_sentiment(user_input)
+            st.success(f'Sentiment Analysis Result: {sentiment_result}')
             
-            if st.button("Analyze Sentiment"):
-                # Preprocess the review
-                preprocessed_review = preprocess_text(review)
-
-                # Analyze Sentiment
-                response = openai.Completion.create(
-                    engine="gpt-3.5-turbo",
-                    prompt=f"The sentiment of this review is: {preprocessed_review}",
-                    temperature=0.5,
-                    max_tokens=100
-                )
-                st.write(response.choices[0].text.strip())
+            # Display Additional NLP Features
+            additional_nlp_features(additional_features)
+            
+            # Display Visualizations
+            display_visualizations(sentiment_result, additional_features)
+            
+            # Collect User Feedback
+            collect_feedback(user_input, sentiment_result, additional_features)
         else:
-            st.info("No reviews have been submitted yet.")
-    else:
-        st.warning("You must enter your OpenAI API Key to use the app.")
+            st.warning('Please enter a review to analyze.')
+
+if __name__ == "__main__":
+    main()
