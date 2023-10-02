62777beada7a18b8b348340da59b4a5298bb0e56
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 07:04:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,58 @@
 import streamlit as st
 import openai
-from preprocessing import preprocess_text  # Importing our custom mdae preprocessing function
+from preprocessing import preprocess_text # Importing our custom-made preprocessing function
 import SessionState
 
-# Initialize SessionState
-session_state = SessionState.get(api_key="")
+# Initialize session state
+session_state = SessionState.get(api_key="", reviews=[])
 
-if not session_state.api_key:
-    # Prompt the user to enter their OpenAI API key
-    session_state.api_key = st.text_input("Enter your OpenAI API Key:")
-    if session_state.api_key:
-        # Set the API key for OpenAI
+# Select Page
+page = st.sidebar.selectbox("Choose a page", ["Enter Review", "Analyze Reviews"])
+
+# Page 1: Enter Review
+if page == "Enter Review":
+    st.header('Enter a new Review')
+
+    # Input for New Review
+    new_review = st.text_area("Enter your review here:")
+    
+    if st.button('Submit Review'):
+        if new_review:
+            session_state.reviews.append(new_review)
+            st.success("Review Submitted Successfully!")
+        else:
+            st.warning("Please enter a review before submitting.")
+
+# Page 2: View and Analyze Reviews
+elif page == "Analyze Reviews":
+    st.header('Analyze Reviews in Real-Time')
+
+    # API Key Input
+    if not session_state.api_key:
+        session_state.api_key = st.text_input("Enter your OpenAI API Key:")
+        if session_state.api_key:
+            openai.api_key = session_state.api_key
+    else:
         openai.api_key = session_state.api_key
-else:
-    openai.api_key = session_state.api_key
-
-def analyze_sentiment(text):
-    response = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=f"This is a review: {text}. The sentiment of this review is:",
-        temperature=0,
-        max_tokens=60
-    )
-    return response.choices[0].text.strip()
-
-def main():
-    st.title('Advanced NLP Sentiment Analysis App')
-    st.sidebar.header('About App')
-    st.sidebar.write('This app uses advanced NLP models (GPT 3.5 Turbo) to analyze sentiments of student reviews.')
-
+    
+    # If API key is set, display and analyze reviews
     if openai.api_key:
-        user_input = st.text_area("Enter the review:")
-        if st.button('Analyze'):
-            if user_input:
-                # Preprocess the input text
-                preprocessed_text = preprocess_text(user_input)
+        if session_state.reviews:
+            review = st.selectbox('Choose a review to analyze:', session_state.reviews)
+            
+            if st.button("Analyze Sentiment"):
+                # Preprocess the review
+                preprocessed_review = preprocess_text(review)
+
                 # Analyze Sentiment
-                result = analyze_sentiment(preprocessed_text)
-                # Display Result
-                st.success(f'Sentiment Analysis Result: {result}')
-                # Advanced Visualization of Result
-                st.bar_chart({
-                    'Sentiment Score': {
-                        'Positive': 1 if 'positive' in result else 0,
-                        'Negative': 1 if 'negative' in result else 0,
-                        'Neutral': 1 if 'neutral' in result else 0
-                    }
-                })
-            else:
-                st.warning('Please enter a review to analyze.')
+                response = openai.Completion.create(
+                    engine="gpt-3.5-turbo",
+                    prompt=f"The sentiment of this review is: {preprocessed_review}",
+                    temperature=0.5,
+                    max_tokens=100
+                )
+                st.write(response.choices[0].text.strip())
+        else:
+            st.info("No reviews have been submitted yet.")
     else:
         st.warning("You must enter your OpenAI API Key to use the app.")
-
-if __name__ == "__main__":
-    main()
