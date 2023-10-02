03a1bc58217b4327ed0980084f45a149239ecd3c
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-02 07:27:33 +0200

    Update sentiment_analysis.py

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,13 +1,26 @@
 import openai
-from some_nlp_library import some_keyword_extraction, some_named_entity_recognition  
+import spacy
+from gensim.summarization import keywords as extract_keywords
+
+# Load SpaCy model
+nlp = spacy.load('en_core_web_sm')
 
 def analyze_sentiment(text):
-    # ... Existing Sentiment Analysis Code ...
-    keywords = some_keyword_extraction(text)
-    entities = some_named_entity_recognition(text)
-    return sentiment_result, {'keywords': keywords, 'entities': entities}
-
-def additional_nlp_features(features):
-    st.subheader('Additional NLP Features:')
-    st.write(f"Keywords: {', '.join(features['keywords'])}")
-    st.write(f"Named Entities: {', '.join(features['entities'])}")
+    # Replace with actual OpenAI API call for Sentiment Analysis.
+    response = openai.Completion.create(
+        engine="gpt-3.5-turbo", 
+        prompt=f"This is a review: {text}. The sentiment of this review is:", 
+        temperature=0, 
+        max_tokens=60
+    )
+    sentiment_result = response.choices[0].text.strip()
+    
+    # Extracting Keywords using Gensim
+    extracted_keywords = extract_keywords(text, words=5).split('\n')
+    
+    # Named Entity Recognition using SpaCy
+    doc = nlp(text)
+    entities = [ent.text for ent in doc.ents]
+    
+    additional_features = {'keywords': extracted_keywords, 'entities': entities}
+    return sentiment_result, additional_features
