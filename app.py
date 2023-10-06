aac59f8a5eff1d372c8904b4d40950df6f68c011
Tadiwanashe Nyaruwata <64325379+tadiwamark@users.noreply.github.com>
2023-10-06 08:45:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import openai
 import SessionState  
 from collections import deque
-#from authentication import authenticate_user
-from sentiment_analysis import analyze_sentiment, additional_nlp_features
+from sentiment_analysis import analyze_sentiment, additional_nlp_features, analyze_sentiment_simple
 from visualization import display_visualizations
 from feedback import collect_feedback
 from user_interface import show_instructions
@@ -12,22 +11,11 @@ import nltk
 def main():
     st.title('Advanced NLP Course Sentiment Analysis App')
     
-    # Authenticate User
-    #user = authenticate_user()
-    
-    # Initialize Session State
-    if 'api_key' not in st.session_state:
-        st.session_state.api_key = ""
-    if 'reviews' not in st.session_state:
-        st.session_state.reviews = deque(maxlen=100)
-    
-    # Get API Key
-    if not st.session_state.api_key:
+    # Get API Key for GPT-3.5
+    if not st.session_state.get('api_key'):
         st.session_state.api_key = st.sidebar.text_input("Enter your OpenAI API Key:")
         if st.session_state.api_key:
             openai.api_key = st.session_state.api_key
-    else:
-        openai.api_key = st.session_state.api_key
 
     # Define Page Layout
     st.sidebar.header('Navigation')
@@ -37,45 +25,50 @@ def main():
     if page == 'Enter Review':
         st.title('Enter Your Review')
         
-        # User Input
         user_input = st.text_area("Paste the course review here:")
         if st.button("Submit Review"):
-            if user_input:  # Prevent empty strings from being added
+            if user_input:
+                if 'reviews' not in st.session_state:
+                    st.session_state.reviews = deque(maxlen=100)
                 st.session_state.reviews.appendleft(user_input)
                 st.success("Review Submitted Successfully!")
             else:
                 st.warning("Review field is empty!")
-    
+
     elif page == 'Analysis':
         st.title('Real-time Review Analysis')
         
-        if st.session_state.reviews:
+        model_choice = st.selectbox('Choose a model', ['GPT-3.5 Turbo', 'Simple Model'])
+
+        if st.session_state.get('reviews'):
             selected_review_index = st.selectbox('Select a review to analyze:', list(range(len(st.session_state.reviews))), format_func=lambda x: st.session_state.reviews[x])
             selected_review = st.session_state.reviews[selected_review_index]
             
             if st.button("Analyze Review"):
-                if openai.api_key:
-                    conversation = [
-                        {"role": "system", "content": "You are a helpful assistant."},
-                        {"role": "user", "content": f"The sentiment of this review is: {selected_review}"}
-                    ]
-                    
-                    try:
-                        response = openai.ChatCompletion.create(
-                            model="gpt-3.5-turbo",
-                            messages=conversation,
-                            temperature=0.5,
-                            max_tokens=100
-                        )
-                        st.write("GPT 3.5-Turbo's Response:", response['choices'][0]['message']['content'])
-                    except openai.error.OpenAIError as e:
-                        st.error(f"Error: {e}")
-                else:
-                    st.error("OpenAI API Key is missing. Please enter the API Key.")
+                if model_choice == 'GPT-3.5 Turbo':
+                    if openai.api_key:
+                        conversation = [
+                            {"role": "system", "content": "You are a helpful assistant."},
+                            {"role": "user", "content": f"The sentiment of this review is: {selected_review}"}
+                        ]
+                        try:
+                            response = openai.ChatCompletion.create(
+                                model="gpt-3.5-turbo",
+                                messages=conversation,
+                                temperature=0.5,
+                                max_tokens=100
+                            )
+                            st.write("GPT 3.5-Turbo's Response:", response['choices'][0]['message']['content'])
+                        except openai.error.OpenAIError as e:
+                            st.error(f"Error: {e}")
+                    else:
+                        st.error("OpenAI API Key is missing. Please enter the API Key.")
+                elif model_choice == 'Simple Model':
+                    sentiment = analyze_sentiment_simple(selected_review)
+                    st.write(f"Simple Model's Sentiment: {sentiment}")
         else:
             st.warning('No reviews have been submitted yet!')
-
-        
+            
     elif page == 'Advanced Analysis':
         # Setup Instructions
         show_instructions()
